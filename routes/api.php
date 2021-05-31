cfbf37eb0578138ea31179fa41f63e6447f7068e
Laratips <aashish201030@gmail.com>
2021-05-31 18:37:52 +0100

    make settings data dynamic

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\ArticlesController;
+use App\Http\Controllers\Api\SettingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('articles', [ArticlesController::class, 'index']);
 Route::get('articles/{article:slug}', [ArticlesController::class, 'show']);
+Route::get('settings', [SettingsController::class, 'index']);
