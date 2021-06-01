ff347a920bc46498a005b513ea154fc794154ebc
Laratips <aashish201030@gmail.com>
2021-06-01 07:10:42 +0100

    complete contact us submission

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\ArticlesController;
+use App\Http\Controllers\Api\ContactUsController;
 use App\Http\Controllers\Api\SettingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('articles', [ArticlesController::class, 'index']);
 Route::get('articles/{article:slug}', [ArticlesController::class, 'show']);
 Route::get('settings', [SettingsController::class, 'index']);
+Route::post('contact-us', [ContactUsController::class, 'store']);
